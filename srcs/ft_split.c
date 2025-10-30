c7cbfec34f7401424e890abe586e62bd25629742
Pedro Henrique Panno Moromizato <pedrohe3@c1r16s4.42porto.com>
2025-10-30 21:10:50 +0000

    Split fixed

--- srcs/ft_split.c
+++ srcs/ft_split.c
@@ -5,85 +5,97 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: pedrohe3 <pedrohe3@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/29 19:31:14 by pedrohe3          #+#    #+#             */
-/*   Updated: 2025/10/29 21:52:23 by pedrohe3         ###   ########.fr       */
+/*   Created: 2025/10/30 18:51:15 by pedrohe3          #+#    #+#             */
+/*   Updated: 2025/10/30 21:07:28 by pedrohe3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_count_words(const char *s, char c)
+static void	ft_free_split(char **split)
+{
+	while (*split)
+	{
+		free(*split);
+		split++;
+	}
+}
+
+static int	ft_countwords(char const *s, char c)
 {
 	int	is_word;
 	int	words;
-	int	i;
 
-	is_word = 0;
 	words = 0;
-	i = -1;
-	while (s[++i])
+	is_word = 0;
+	while (*s)
 	{
-		if(is_word && s[i] == c)
+		if (!is_word && *s != c)
+			is_word = 1;
+		else if (is_word && *s == c)
 		{
 			is_word = 0;
 			words++;
 		}
-		else if (s[i] != c)
-			is_word = 1;
+		s++;
 	}
 	if (is_word)
 		words++;
 	return (words);
 }
 
+static int	ft_sep_strlen(char const *s, char c)
+{
+	size_t	len;
+
+	len = 0;
+	while (*s)
+	{
+		if (*s != c)
+			len++;
+		else if (len > 0 && *s == c)
+			break ;
+		s++;
+	}
+	return (len);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	char	**split;
-	char	*dup;
-	char	*aux;
-	int	i;
+	size_t	len;
+	int		words;
+	int		i;
 
-	split = 0;
-	dup = ft_strdup(s);
-	aux = 0;
-	i = 0;
-	split = (char **)malloc(sizeof(char *) * ft_count_words(s, c));
+	words = ft_countwords(s, c);
+	split = (char **)ft_calloc(sizeof(char *), words + 1);
 	if (!split)
 		return (NULL);
-	while (ft_strrchr(dup, c))
+	i = -1;
+	while (++i < words)
 	{
-		aux = ft_strrchr(dup, c);
-		if (ft_strlen(aux) > 1)
-			split[i++] = ft_strdup(aux + 1);
-		ft_strlcpy(aux, "", 1);
+		while (*s == c)
+			s++;
+		len = ft_sep_strlen(s, c);
+		split[i] = ft_substr(s, 0, len);
+		if (!split[i])
+			return (ft_free_split(split), NULL);
+		s += len;
 	}
-	split[i] = ft_strdup(dup);
-	free(dup);
-	dup = 0;
 	return (split);
 }
 /*
 int	main(void)
 {
 	char	**arr;
-	char	*s = " Hello World ";
-	int	i;
-	int	count;
+	char	*str = "help!";
+	char	sep = ' ';
 
-	i = -1;
-	count = ft_count_words(s, ' ');
-	arr = ft_split(s, ' ');
-	printf("\n%d words in \"%s\"\n", count, s);
-	while (++i < count)
+	arr = ft_split(str, sep);
+	while (*arr)
 	{
-		printf("len:%ld s%d: %s", ft_strlen(arr[i]), i, arr[i]);
-		if (arr[i][ft_strlen(arr[i])] == '\0')
-			printf("\\0\n");
-		free(arr[i]);
-		arr[i] = 0;
+		printf("arr: %s\n", *arr);
+		arr++;
 	}
-	free(arr);
-	arr = 0;
 	return (0);
-}
-*/
+}*/
--- srcs/ft_strjoin.c
+++ srcs/ft_strjoin.c
@@ -1,7 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pedrohe3 <pedrohe3@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/30 13:42:06 by pedrohe3          #+#    #+#             */
+/*   Updated: 2025/10/30 14:02:33 by pedrohe3         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
 	size_t	len_s1;
 	size_t	len_s2;
@@ -11,7 +22,7 @@ char *ft_strjoin(char const *s1, char const *s2)
 	len_s1 = ft_strlen(s1);
 	len_s2 = ft_strlen(s2);
 	join = (char *)malloc(sizeof(char) * (len_s1 + len_s2 + 1));
-	ft_strlcat(join, s1, len_s1 + 1);
+	ft_strlcpy(join, s1, len_s1 + 1);
 	ft_strlcat(join, s2, len_s1 + len_s2 + 1);
 	return (join);
 }
